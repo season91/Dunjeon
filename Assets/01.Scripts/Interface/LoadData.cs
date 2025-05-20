6df482e2928e2868867d70f3004e7ad1f4fe460f
season91 <choayoung91@naver.com>
2025-05-20 11:32:54 +0900

    [feat] Player InputSystem Look, Jump 추가

--- Assets/01.Scripts/Interface/LoadData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class LoadData : MonoBehaviour
-{
-    // 전체 로드 실행
-    private void OnEnable() => GameManager.RaiseLoad();
-    private void Awake() => GameManager.RaiseAwake();
-    private void Start() => GameManager.RaiseStart();
-}
